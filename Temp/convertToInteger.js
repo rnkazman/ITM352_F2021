14644d73b26c240be2a3daa65be3a7d206f0dbb7
Rick Kazman <kazman@hawaii.edu>
2021-09-29 11:28:00 -0700

    xxx

---
+++ Temp/convertToInteger.js
@@ -0,0 +1,9 @@
+var what = 39.000000000000001;
+
+var what_int = Math.floor(what);
+
+if (what == what_int) {
+    console.log(`${what} is an integer`);    
+} else {
+    console.log(`${what} is not an integer`);  
+}
